8e18dfc3aacc663a47f30214c7000f18bca67f7e
AjithGWC <Ajith.Kumar@gwcdata.ai>
2025-05-06 14:21:34 +0530

    Update card.py

--- card.py
+++ card.py
@@ -16,7 +16,10 @@ def create_card():
         body = data.get('body')
         access_token = data.get('access_token')
         refer = data.get('refer')
-        # print(".........",cookie_value, body, access_token, refer)
+        print(".........",cookie_value)
+        print(body)
+        print(access_token)
+        print(refer)
 
         if not cookie_value or not body:
             return jsonify({"error": "Both 'cookie' and 'body' fields are required"}), 400
