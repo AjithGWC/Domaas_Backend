a2d1790d6eaa5fa2ba9bc8f1b7d85241712844a5
AjithGWC <Ajith.Kumar@gwcdata.ai>
2025-05-06 12:26:27 +0530

    Update card.py

--- card.py
+++ card.py
@@ -36,7 +36,7 @@ def create_card():
 
         # Send request 
         domo_response = requests.put(url, headers=headers, data=json.dumps(body))
-        print("....", domo_response)
+        print("....", domo_response.json())
 
         # Return response
         return jsonify({
